9273d31eb7ad883efeda3ba40920f96b32965c8c
Baptiste <baptiste.colin@outlook.com>
2017-08-05 17:04:22 +0900

    formatting

--- scripts/launch_sync.sh
+++ scripts/launch_sync.sh
@@ -5,7 +5,5 @@
 # --networkid est suivi de l'ID de la blockchain à laquelle on veut se connecter.
 #
 # Cette commande permet donc de se connecter à la blockchain d'ID 19031783, et rangera les données téléchargées dans ~/.ethereum/minecoin
-#
-# NB : 19031783, comme le 19 mars 1783, jour de création de l'Ecole des Mines de Paris par arrêt du Conseil de Roi Louis XVI
 
 geth --datadir ~/.ethereum/minecoin --networkid 19031783
